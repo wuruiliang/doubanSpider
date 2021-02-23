97f63e42bac82f6093a67b751db377cb0be51445
ruiliangwu@yangqianguan.com <ruiliangwu@yangqianguan.com>
2021-02-23 12:05:44 +0800

    整理入参

--- doubanSpider/spiders/douban.py
+++ doubanSpider/spiders/douban.py
@@ -6,23 +6,24 @@ from doubanSpider.items import DoubanspiderItem
 class DoubanSpider(scrapy.Spider):
     name = 'douban'
     allowed_domains = ['www.douban.com']
-    start_urls = ['http://www.douban.com/group/26926/discussion']
-    max_page = 5
-    search_str_list = ['6号线', '十号线']
+    start_urls = []
+    max_page = 50
+    search_str_list = []
     proxy = None
     count = 0
 
-    def __init__(self, start_url=None, max_page=None, search_strs='', proxy=None, *args, **kwargs):
+    def __init__(self, start_url=None, max_page=None, search_strs=None, proxy=None, *args, **kwargs):
         super(DoubanSpider, self).__init__(*args, **kwargs)
         if start_url is None:
-            start_url = 'http://www.baidu.com'
+            start_url = 'https://www.baidu.com'
         self.start_urls = [start_url]
         self.max_page = max_page
-        self.search_str_list = search_strs.split(",")
+        self.search_str_list = search_strs.split(",") if search_strs is not None else None
         self.proxy = proxy
 
     def parse(self, response):
         if self.start_urls is None or self.max_page is None or self.search_str_list is None or not self.search_str_list:
+            print("参数错误！！！")
             return None
         items = []
         for each in response.xpath('//*/table[@class="olt"]/tr'):
