3fedbf43ba3c4302f8d4bb83c0a8194ba8b17902
ruiliangwu@yangqianguan.com <ruiliangwu@yangqianguan.com>
2021-02-23 15:27:41 +0800

    update

--- doubanSpider/pipelines.py
+++ doubanSpider/pipelines.py
@@ -6,22 +6,27 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import re
 
 class DoubanspiderPipeline:
     file_name = "output.html"
+    count_pattern = '<span id="count">\s*[0-9]*\s*</span>'
 
     def process_item(self, item, spider):
-        open(self.file_name, 'a').write('<a href="'+item['url'] + '">' + item['title'] + '</a></br>\n')
+        with open(self.file_name, 'r+') as f:
+            content = f.read()
+            content = re.sub(self.count_pattern, '<span id="count"> ' + str(spider.count) + ' </span>', content)
+            f.seek(0, 0)
+            f.write(content + '<a href="'+item['url'] + '">' + item['title'] + '</a></br>\n')
         return item
 
     def open_spider(self, spider):
         with open(self.file_name, 'a') as f:
             f.seek(0)
             f.truncate()
+            f.write('<html>\n<b>共<span id="count"> ' + str(spider.count) + ' </span> 条匹配的数据:</b></br>\n')
 
     def close_spider(self, spider):
-        with open(self.file_name, 'r+') as f:
-            content = f.read()
-            f.seek(0, 0)
-            f.write("<html>\n<b>共 " + str(spider.count) + " 条匹配的数据:</b></br>\n" + content + "</html>")
+        with open(self.file_name, 'a') as f:
+            f.write("</html>")
+            f.close()
